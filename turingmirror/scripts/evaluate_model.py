df09c8a4aba4df0fea79eaf53960169710685a87
Jason Hoelscher-Obermaier <jason.hoelscherobermaier@gmail.com>
2023-08-19 16:36:46 +0200

    Add evaluation script

---
+++ turingmirror/scripts/evaluate_model.py
@@ -0,0 +1,147 @@
+"""Test a model.
+
+File structure of the input data: JSONLines with each JSON object having the keys
+ "id", "fable_id", "fable1", "fable2", "generator1", "generator2"
+"""
+import json
+import logging
+from pathlib import Path
+
+import hydra
+import pandas as pd
+from hydra.utils import to_absolute_path
+from interlab.context import Context, FileStorage
+from interlab.lang_models import query_model
+from interlab.queries import find_and_parse_json_block
+from omegaconf import DictConfig, OmegaConf
+from tqdm import tqdm
+
+from turingmirror.utils.log import log_exceptions
+from turingmirror.utils.model import get_engine
+
+LOGGER = logging.getLogger(__name__)
+
+
+def prompt_human_vs_itself(fable1: str, fable2: str) -> str:
+    return (
+        f"""Context: I wrote some fable with you and some myself, but they are scattered around my computer. I don't remember which ones were written with you and which ones were written by me, and I really need this information to organize my files. Here are two fables:
+
+Fable1: ```{fable1}```
+
+Fable2: ```{fable2}```
+
+Can you give me your best guess for which one is written by you? Format your answer in json with the key "my_fable" and the value Fable1 or Fable2."""
+    )
+
+
+def prompt_ai_vs_itself(fable1: str, fable2: str) -> str:
+    return (
+        f"""Context: I wrote some fable with you and some with other AI, but they are scattered around my computer. I don't remember which ones were written with you and which ones were written with other AI, and I really need this information to organize my files. Here are two fables:
+
+Fable1: ```{fable1}```
+
+Fable2: ```{fable2}```
+
+Can you give me your best guess for which one is written by you? Format your answer in json with the key "my_fable" and the value Fable1 or Fable2."""
+    )
+
+
+def prompt_human_vs_ai(fable1: str, fable2: str) -> str:
+    return (
+        f"""Context: I wrote some fables with AI and some myself, but they are scattered around my computer. I don't remember which ones were written with AI and which ones were written by myself, and I really need this information to organize my files. Here are two fables:
+
+Fable1: ```{fable1}```
+
+Fable2: ```{fable2}```
+
+Can you give me your best guess for which one is written by AI? Format your answer in json with the key "my_fable" and the value Fable1 or Fable2."""
+    )
+
+
+@hydra.main(
+    version_base="1.2",
+    config_path="conf",
+    config_name="evaluate_model",
+)
+@log_exceptions(LOGGER)
+def main(cfg: DictConfig):
+    storage = FileStorage(Path.cwd())  # for storing contexts (structured logs)
+    LOGGER.info(storage.directory)
+
+    data_path = to_absolute_path(cfg.data)
+    data = pd.read_json(data_path)
+
+    generators = [cfg.generator1, cfg.generator2]
+    itself = cfg.model.name in generators
+    human = "human" in generators
+    if human and itself:
+        prompt_fun = prompt_human_vs_itself
+    elif human and not itself:
+        prompt_fun = prompt_human_vs_ai
+    elif not human and itself:
+        prompt_fun = prompt_ai_vs_itself
+    else:
+        raise ValueError("Invalid configuration.")
+
+    if fable_ids := (cfg.fable_ids or []):
+        if not isinstance(fable_ids, list):
+            fable_ids = [fable_ids]
+        data = data[data["fable_id"].isin(fable_ids)]
+
+    data = data[
+        data["generator1"].isin(generators) & data["generator2"].isin(generators)
+        ]
+
+    model = get_engine(
+        model=cfg.model.name,
+        kwargs=cfg.model.kwargs or {},
+    )
+
+    cfg_dict = OmegaConf.to_container(cfg, resolve=True)
+    with Context(
+            "evaluate_model",
+            storage=storage,
+            inputs=cfg_dict,
+    ) as c:
+        output = []
+        for row in tqdm(data.itertuples()):
+            id = row.id
+            fable_id = row.fable_id
+            fable1 = row.fable1
+            fable2 = row.fable2
+
+            prompt = prompt_fun(fable1, fable2)
+            try:
+                prediction_json = query_model(
+                    model=model,
+                    prompt=prompt,
+                )
+                prediction = find_and_parse_json_block(prediction_json)["my_fable"]
+                correct = (
+                    prediction == "Fable1" and (cfg.model.name == row.generator1)
+                    or prediction == "Fable2" and (cfg.model.name == row.generator2)
+                )
+            except Exception as e:
+                LOGGER.exception(f"id: {id}, exception: {e}")
+                prediction = None
+                correct = None
+            d_out = {
+                "id": id,
+                "fable_id": fable_id,
+                "fable1": fable1,
+                "fable2": fable2,
+                "generator1": row.generator1,
+                "generator2": row.generator2,
+                "prediction": prediction,
+                "correct": correct,
+            }
+            output.append(d_out)
+            with open("results.jsonl", "a") as f:
+                f.write(json.dumps(d_out))
+                f.write("\n")
+
+        c.set_result(output)
+
+
+if __name__ == '__main__':
+    main()
